addb4ee6013975d07f8dd64b82c38c8a66fdec9c
Gopi Krishna Saravanan <gopikrishna.saravanan@cegeka.com>
2025-08-25 16:00:20 +0530

    changed index.js in that changed browserrouting to hashrouting

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import reportWebVitals from './reportWebVitals.js';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { StoreContextProvider } from './context/globalstorage.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<>
 		<StoreContextProvider>
-			<BrowserRouter basename="/zoon">
+			<HashRouter>
 				<App />
-			</BrowserRouter>
+			</HashRouter>
 		</StoreContextProvider>
 	</>
 );
