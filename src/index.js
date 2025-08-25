a7eb4bfb31bb47835247aa893465ea7ca3f00a75
Gopi Krishna Saravanan <gopikrishna.saravanan@cegeka.com>
2025-08-25 15:42:41 +0530

    changed basename browerrouter

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<>
 		<StoreContextProvider>
-			<BrowserRouter>
+			<BrowserRouter basename="/zoon">
 				<App />
 			</BrowserRouter>
 		</StoreContextProvider>
